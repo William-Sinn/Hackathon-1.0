b586011e48df1554936c0385c9ef9d89537d2e56
William-Sinn <sinnwilliam7@gmail.com>
2022-04-09 09:40:42 -0400

    Created classes for text accumulation, video creation, and a main file to tie it all together

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ import AccumulateText as AText
 import VideoMaker as VMaker
 
 if __name__ == '__main__':
+    # creates scraper and scrapes specified number of submissions
     scraper = RScrape.RedditScraper("AskReddit", 1)
     submission_list = scraper.scrape_submissions(2, 2)
+
+    # accumulates the text into easily TTSed format
     accumulator = AText.AccumulateText(submission_list[0])
+
+    # takes formatted text and creates video using it and a stock video for background
     video_maker = VMaker.VideoMaker("Stock/1.mp4")
+
+    # creates a video for title, text, top, and controversial comments
     video_maker.create_video_standard(accumulator.accumulate_text_standard())
     video_maker.create_video_top(accumulator.accumulate_text_top_reply()[0])
     video_maker.create_video_controversial(accumulator.accumulate_text_controversial_reply()[0])
+
+    # All Done!!! :)
     print("All Done!")
